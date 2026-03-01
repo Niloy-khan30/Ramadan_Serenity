2ddbf81ea2f5c55d1c12a0557b7804c5e5396c46
Niloy-Khan30 <kamrul.hassan.niloy@g.bracu.ac.bd>
2026-03-01 20:47:29 +0600

    frontend

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Sidebar from '../Navbar/sidebar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
